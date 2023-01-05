66f9b788c7c4dd021df59a0e4846d148953c98fc
rawi daniel <rawidaniel96@gmail.com>
2023-01-05 19:46:29 +0000

    fix bug

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,5 +6,6 @@ from typing import List, Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """reterive tuple object"""
     result: Tuple[str, float] = (k, v ** 2)
     return result
