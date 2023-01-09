5167583c6431e80f4902aca58a3ead6e173c09a2
Rawi Daniel <rawidaniel96@gmail.com>
2023-01-09 13:11:13 +0300

    fix output error

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,8 +7,12 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
-    """Reterive a random number between 0 and max_dely after some delay"""
-    num = random.uniform(0, max_delay)
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    max_delay - awit time
+
+    Reterive a random number between 0 and max_dely after some delay
+    """
+    num: float = random.uniform(0, max_delay)
     await asyncio.sleep(num)
     return num
