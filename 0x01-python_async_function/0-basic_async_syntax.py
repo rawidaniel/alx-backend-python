fb142db06f2f429e5131bbc52f596aac7e148c5a
Rawi Daniel <rawidaniel96@gmail.com>
2023-01-09 13:01:37 +0300

    asynchronous code

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Module 0-basic_async_syntax
+"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay= 10):
+    """Reterive a random number between 0 and max_dely after some delay"""
+    num = random.uniform(0, max_delay)
+    await asyncio.sleep(num)
+    return num
