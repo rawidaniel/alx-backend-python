d91f0d189d0b65af36e272db4237a8f09a8a902f
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-03 12:18:43 +0300

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -23,7 +23,7 @@ class TestGithubOrgClient(unittest.TestCase):
         org: str
           organization name
         mock_get_json: mock object
-
+            mock object of get_json
         """
         githubClient = GithubOrgClient(org)
         test_response = githubClient.org
@@ -43,6 +43,27 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(test_response,
                              mock_org.return_value.get("repos_url"))
             mock_org.assert_called_once()
+
+    @patch("client.get_json", return_value=[{'name': 'Holberton'},
+                                            {'name': '89'},
+                                            {'name': 'alx'}])
+    def test_public_repos(self, mock_get_json):
+        """Test the result of public_repos as expected
+
+        mock_get_json: mock object
+            mock object of get_json
+        """
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mock_public_repos:
+            test_payload = "https://api.github.com/orgs/google/repos"
+            mock_public_repos.return_value = test_payload
+            githubOrgClient = GithubOrgClient("google")
+            test_result = githubOrgClient.public_repos()
+            self.assertEqual(test_result,
+                             [obj["name"]
+                              for obj in mock_get_json.return_value])
+            mock_get_json.assert_called_once()
+            mock_public_repos.assert_called_once()
 
 
 if __name__ == "__main__":
